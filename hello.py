375cf8152ee23533edc0232b021c6dba066b6eec
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-05-18 03:24:58 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -9,3 +9,18 @@ if spam_amount > 0:
 
 viking_song = "Spam " * spam_amount #operator overloading
 print(viking_song)
+
+# Numbers and arithmetic in Python
+type(spam_amount) #int
+type(19.95) #float
+
+a = 4
+b = 2
+
+print(a/b) # true division = 2
+print(a//b) # floor division = 
+print(a%b) # modulus: Integer remainder after division of a by b
+print(a**b) # 4*4 Exponential: a raised to the power of b
+print(-a) # Negation: The negative of a
+
+# PEMDAS - Parentheses, Exponents, Multiplication/Division, Addition/Subtraction. 9ja BODMAS 😒
