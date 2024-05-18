3caa2bcc78120dca895561feed80b97188f9768e
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-05-18 03:10:29 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,11 @@
-print("hello world!")
+spam_amount = 0
+print(spam_amount)
+
+# Ordering Spam, egg, Spam, Spam, bacon and Spam (4 more servings of Spam)
+spam_amount = spam_amount * 4
+
+if spam_amount > 0:
+    print("But I don't want any Spam!!") #indentation indictes that all indented code belongs to the conditional above:
+
+viking_song = "Spam " * spam_amount #operator overloading
+print(viking_song)
