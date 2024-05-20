d74c5f57e8afa3d08fe802395d987ac8e65e3dac
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-05-20 01:18:18 +0100

    Update funtions.py

--- funtions.py
+++ funtions.py
@@ -3,3 +3,21 @@ import sys
 fp = sys.stdout
 print("Do you want to continue (Y/n): ", end="")
 fp.flush()  # Uncomment this line to see the prompt immediately
+
+import time
+while True:
+    #print("great output as always")
+    print("great output as always", flush=True)
+    #time.sleep(2)
+    break
+
+def least_difference(a, b, c):
+    diff1 = abs(a - b)
+    diff2 = abs(b - c)
+    diff3 = abs(a - c)
+    return min(diff1, diff2, diff3)
+
+print(least_difference(1, 10, 100))
+
+# print(help(time))
+help(print)
