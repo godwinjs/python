53d5ca672b614d7fe8483032f0489376b9d47d59
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-05-27 09:00:09 +0100

    Update list_comprehension.py

--- list_comprehension.py
+++ list_comprehension.py
@@ -1,3 +1,4 @@
 planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Jupiter', 'Saturn', 'Uranus', 'Neptune']
-short_planets = [string for string in planets if len(string) < 6]
+# variable = [ expression for item in iterable if condition]
+short_planets = [ 3 for string in planets if len(string) < 6]
 print(short_planets)
