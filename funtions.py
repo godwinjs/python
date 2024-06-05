d6a1d0d9c1369a55ced6464126da152d1b575e0b
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-06-05 15:19:59 +0100

    Update funtions.py

--- funtions.py
+++ funtions.py
@@ -134,16 +134,6 @@ def should_hit(dealer_total, player_total, player_low_aces, player_high_aces):
     """
     return False
 
-def should_hit(dealer_total, player_total, player_low_aces, player_high_aces):
-    """Return True if the player should hit (request another card) given the current game
-    state, or False if the player should stay.
-    When calculating a hand's total value, we count aces as "high" (with value 11) if doing so
-    doesn't bring the total above 21, otherwise we count them as low (with value 1). 
-    For example, if the player's hand is {A, A, A, 7}, we will count it as 11 + 1 + 1 + 7,
-    and therefore set player_total=20, player_low_aces=2, player_high_aces=1.
-    """
-    return False
-
 
 x = 12
 # x is a real number, so its imaginary part is 0.
