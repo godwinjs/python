a6b2a31a1e91205651fd5ed68a1faf514315cb19
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-06-07 02:44:45 +0100

    List comp. exercises

--- hello.py
+++ hello.py
@@ -49,3 +49,60 @@ print(int('807') + 1)
 
 #
 print(int(min(10.23, 10.54, 10.66, 10.74, 10.99, 10.55)))
+
+#RECORD
+# Initialize the list to store the total scores for each student
+total_scores = []
+
+# Number of students
+num_students = int(input("Enter the number of students: "))
+
+# Number of subjects
+num_subjects = int(input("Enter the number of subjects: "))
+
+# Collect scores for each student
+for i in range(num_students):
+    print(f"\nEntering scores for Student {i + 1}")
+    total_score = 0
+    for j in range(num_subjects):
+        score = float(input(f"Enter score for subject {j + 1}: "))
+        total_score += score
+    total_scores.append(total_score)
+
+# Calculate and print the average score for each student
+print("\nAverage scores for each student:")
+for i in range(num_students):
+    average_score = total_scores[i] / num_subjects
+    print(f"Student {i + 1}: {average_score:.2f}")
+
+#END_RECORD
+
+#RECORD
+def has_lucky_number(nums):
+    """Return whether the given list of numbers is lucky. A lucky list contains
+    at least one number divisible by 7.
+    """
+    boolean = False
+    # return [num for num in nums if (num % 7) == 0]
+    if len(nums) == 0:
+        return boolean
+
+    for num in nums:
+        if num % 7 == 0:
+            boolean = True
+            break
+        else:
+            boolean = False
+        print(num)
+    return boolean
+#END_RECORD
+def has_lucky_number(nums):
+    """Return whether the given list of numbers is lucky. A lucky list contains
+    at least one number divisible by 7.
+    """
+    return any([num % 7 == 0 for num in nums])
+        
+        
+
+print(has_lucky_number([3, 14]))
+#RECORD
--- test_run.py
+++ test_run.py
@@ -1,23 +1,9 @@
-# Initialize the list to store the total scores for each student
-total_scores = []
+def has_lucky_number(nums):
+    """Return whether the given list of numbers is lucky. A lucky list contains
+    at least one number divisible by 7.
+    """
+    return any([num % 7 == 0 for num in nums])
+        
+        
 
-# Number of students
-num_students = int(input("Enter the number of students: "))
-
-# Number of subjects
-num_subjects = int(input("Enter the number of subjects: "))
-
-# Collect scores for each student
-for i in range(num_students):
-    print(f"\nEntering scores for Student {i + 1}")
-    total_score = 0
-    for j in range(num_subjects):
-        score = float(input(f"Enter score for subject {j + 1}: "))
-        total_score += score
-    total_scores.append(total_score)
-
-# Calculate and print the average score for each student
-print("\nAverage scores for each student:")
-for i in range(num_students):
-    average_score = total_scores[i] / num_subjects
-    print(f"Student {i + 1}: {average_score:.2f}")
+print(has_lucky_number([3, 14]))
