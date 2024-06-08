ca051864715d5d5cb8d6d7c2cf9cf84d31df64ba
Godwin Iyke <godwinikechukwu.dev@gmail.com>
2024-06-08 14:28:59 +0100

    2.

--- hello.py
+++ hello.py
@@ -191,3 +191,38 @@ my_array = arr.array('i', [1, 2, 3, 4, 5])
 print(type(arr))
 #END_RECORD
 
+#RECORD
+def best_items(racers):
+    """Given a list of racer dictionaries, return a dictionary mapping items to the number
+    of times those items were picked up by racers who finished in first place.
+    """
+    winner_item_counts = {}
+    for i in range(len(racers)):
+        # The i'th racer dictionary
+        racer = racers[i]
+        # We're only interested in racers who finished in first
+        if racer['finish'] == 1:
+            for i in racer['items']:
+                # Add one to the count for this item (adding it to the dict if necessary)
+                if i not in winner_item_counts:
+                    winner_item_counts[i] = 0
+                winner_item_counts[i] += 1
+
+        # Data quality issues :/ Print a warning about racers with no name set. We'll take care of it later.
+        if racer['name'] is None:
+            print("WARNING: Encountered racer with unknown name on iteration {}/{} (racer = {})".format(
+                i+1, len(racers), racer['name'])
+                 )
+    return winner_item_counts
+
+print(best_items([
+    {'name': 'Peach', 'items': ['green shell', 'banana', 'green shell',], 'finish': 3},
+    {'name': 'Bowser', 'items': ['green shell',], 'finish': 1},
+    # Sometimes the racer's name wasn't recorded
+    {'name': None, 'items': ['mushroom',], 'finish': 2},
+    {'name': 'Toad', 'items': ['green shell', 'mushroom'], 'finish': 1},
+]) )
+
+#END_RECORD
+
+
